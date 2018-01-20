1a7d5893097be28356887917f0bc0f8c6e5cb452
imzhangtianyi <35641118+imzhangtianyi@users.noreply.github.com>
2018-01-20 18:23:45 -0500

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,5 @@
+# import feature class: 
+
+# dispersion_features includes number of times when zeta crosses 0: zeta0; number of times when the derivative of zeta crosses 0: dz;shelf width: sb; turning point of wave structure: wb
+
+# dispersion_model_features includes number of times when zeta crosses 0: zeta0; number of times when the derivative of zeta crosses 0: dz;turning point of wave structure comparing to shelf width: wb
